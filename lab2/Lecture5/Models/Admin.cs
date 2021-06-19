48f75f22f1a608f1bb9a4cc13b90cf94a8db1849
srmbshishir <srmb.shishir@gmail.com>
2021-06-19 14:30:21 +0600

    check

---
+++ lab2/Lecture5/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lecture5.Models
+{
+            public int Id { get; set; }
+            public string Name { get; set; }
+            [Required]
+            public string Username { get; set; }
+            [Required]
+            public string Password { get; set; }
+        }
+    }
+}
+}
